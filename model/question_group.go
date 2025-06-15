79c4ccdea7a9dec850415d8b884b37051bfb9eb6
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-06-15 11:30:05 +0900

    `QuestionGroup`の`Description`をnullableに

--- model/question_group.go
+++ model/question_group.go
@@ -8,9 +8,10 @@ import (
 
 type QuestionGroup struct {
 	gorm.Model
-	CampID      uint
 	Name        string
-	Description string
+	Description *string
 	Due         time.Time
 	Questions   []Question
+
+	CampID uint
 }
