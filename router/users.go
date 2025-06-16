9154861ff22c61a9692625453d864f1134539d1e
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-06-16 14:24:07 +0900

    `GetStaffs`関数をusers.goに移動

--- router/users.go
+++ router/users.go
@@ -26,3 +26,23 @@ func (s *Server) GetMe(e echo.Context, params GetMeParams) error {
 
 	return e.JSON(http.StatusOK, &response)
 }
+
+func (s *Server) GetStaffs(e echo.Context) error {
+	staffs, err := s.repo.GetStaffs()
+
+	if err != nil {
+		e.Logger().Errorf("failed to get staffs: %v", err)
+
+		return echo.NewHTTPError(http.StatusInternalServerError, "Internal server error")
+	}
+
+	var response []UserResponse
+
+	if err := copier.Copy(&response, &staffs); err != nil {
+		e.Logger().Errorf("failed to copy staffs: %v", err)
+
+		return echo.NewHTTPError(http.StatusInternalServerError, "Internal server error")
+	}
+
+	return e.JSON(http.StatusOK, &response)
+}
