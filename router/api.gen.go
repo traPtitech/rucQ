6c5405cff437f85fd576bae93debda6c586518b0
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-06-24 10:57:13 +0900

    CampのスキーマにdateStartとdateEndフィールドを追加

--- router/api.gen.go
+++ router/api.gen.go
@@ -156,23 +156,27 @@ type AnswerResponse struct {
 
 // CampRequest defines model for CampRequest.
 type CampRequest struct {
-	Description        string `json:"description"`
-	DisplayId          string `json:"displayId"`
-	IsDraft            bool   `json:"isDraft"`
-	IsPaymentOpen      bool   `json:"isPaymentOpen"`
-	IsRegistrationOpen bool   `json:"isRegistrationOpen"`
-	Name               string `json:"name"`
+	DateEnd            openapi_types.Date `json:"dateEnd"`
+	DateStart          openapi_types.Date `json:"dateStart"`
+	Description        string             `json:"description"`
+	DisplayId          string             `json:"displayId"`
+	IsDraft            bool               `json:"isDraft"`
+	IsPaymentOpen      bool               `json:"isPaymentOpen"`
+	IsRegistrationOpen bool               `json:"isRegistrationOpen"`
+	Name               string             `json:"name"`
 }
 
 // CampResponse defines model for CampResponse.
 type CampResponse struct {
-	Description        string `json:"description"`
-	DisplayId          string `json:"displayId"`
-	Id                 int    `json:"id"`
-	IsDraft            bool   `json:"isDraft"`
-	IsPaymentOpen      bool   `json:"isPaymentOpen"`
-	IsRegistrationOpen bool   `json:"isRegistrationOpen"`
-	Name               string `json:"name"`
+	DateEnd            openapi_types.Date `json:"dateEnd"`
+	DateStart          openapi_types.Date `json:"dateStart"`
+	Description        string             `json:"description"`
+	DisplayId          string             `json:"displayId"`
+	Id                 int                `json:"id"`
+	IsDraft            bool               `json:"isDraft"`
+	IsPaymentOpen      bool               `json:"isPaymentOpen"`
+	IsRegistrationOpen bool               `json:"isRegistrationOpen"`
+	Name               string             `json:"name"`
 }
 
 // DashboardResponse defines model for DashboardResponse.
