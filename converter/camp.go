ad9677bc280c225cd9a07e6af1cf2abaf10a7385
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-06-27 15:02:08 +0900

    Campの日付を正しく変換できるように

---
+++ converter/camp.go
@@ -0,0 +1,57 @@
+package converter
+
+import (
+	"errors"
+
+	"github.com/jinzhu/copier"
+	"github.com/oapi-codegen/runtime/types"
+
+	"github.com/traP-jp/rucQ/backend/api"
+	"github.com/traP-jp/rucQ/backend/model"
+)
+
+var campSchemaToModel = copier.TypeConverter{
+	SrcType: api.CampRequest{},
+	DstType: model.Camp{},
+	Fn: func(src any) (any, error) {
+		srcCamp, ok := src.(api.CampRequest)
+
+		if !ok {
+			return nil, errors.New("src is not an api.CampRequest")
+		}
+
+		var dst model.Camp
+
+		if err := copier.Copy(&dst, &srcCamp); err != nil {
+			return nil, err
+		}
+
+		dst.DateStart = srcCamp.DateStart.Time
+		dst.DateEnd = srcCamp.DateEnd.Time
+
+		return dst, nil
+	},
+}
+
+var campModelToSchema = copier.TypeConverter{
+	SrcType: model.Camp{},
+	DstType: api.CampResponse{},
+	Fn: func(src any) (any, error) {
+		srcCamp, ok := src.(model.Camp)
+
+		if !ok {
+			return nil, errors.New("src is not a model.Camp")
+		}
+
+		var dst api.CampResponse
+
+		if err := copier.Copy(&dst, &srcCamp); err != nil {
+			return nil, err
+		}
+
+		dst.DateStart = types.Date{Time: srcCamp.DateStart}
+		dst.DateEnd = types.Date{Time: srcCamp.DateEnd}
+
+		return dst, nil
+	},
+}
---
+++ converter/convert.go
@@ -0,0 +1,16 @@
+package converter
+
+import "github.com/jinzhu/copier"
+
+func Convert[T any](src any) (T, error) {
+	var dst T
+
+	err := copier.CopyWithOption(&dst, src, copier.Option{
+		Converters: []copier.TypeConverter{
+			campSchemaToModel,
+			campModelToSchema,
+		},
+	})
+
+	return dst, err
+}
