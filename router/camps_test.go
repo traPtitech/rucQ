052af04b858824cf3ba78ea4c24ce7cf7ba87d5d
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-06-27 13:31:00 +0900

    Camp取得のテストを追加

---
+++ router/camps_test.go
@@ -0,0 +1,62 @@
+package router
+
+import (
+	"net/http"
+	"testing"
+	"time"
+
+	"gorm.io/gorm"
+
+	"github.com/gavv/httpexpect/v2"
+	"github.com/traP-jp/rucQ/backend/model"
+	"github.com/traP-jp/rucQ/backend/testutil/random"
+)
+
+func TestGetCamps(t *testing.T) {
+	t.Parallel()
+
+	t.Run("Success", func(t *testing.T) {
+		t.Parallel()
+
+		h := setup(t)
+		dateStart := random.Time(t)
+		dateEnd := dateStart.Add(time.Duration(random.PositiveInt(t)))
+		camp := model.Camp{
+			Model: gorm.Model{
+				ID: uint(random.PositiveInt(t)),
+			},
+			DisplayID:          random.AlphaNumericString(t, 10),
+			Name:               random.AlphaNumericString(t, 20),
+			Description:        random.AlphaNumericString(t, 100),
+			IsDraft:            random.Bool(t),
+			IsPaymentOpen:      random.Bool(t),
+			IsRegistrationOpen: random.Bool(t),
+			DateStart:          dateStart,
+			DateEnd:            dateEnd,
+		}
+
+		h.repo.MockCampRepository.EXPECT().GetCamps().Return([]model.Camp{camp}, nil)
+
+		res := httpexpect.WithConfig(httpexpect.Config{
+			BaseURL:  h.server.URL,
+			Reporter: httpexpect.NewAssertReporter(t),
+		}).GET("/api/camps").Expect().Status(http.StatusOK).JSON().Array()
+
+		res.Length().IsEqual(1)
+
+		val := res.Value(0).Object()
+
+		val.Keys().ContainsOnly(
+			"id", "displayId", "name", "description", "isDraft", "isPaymentOpen",
+			"isRegistrationOpen", "dateStart", "dateEnd")
+		val.Value("id").Number().IsEqual(camp.ID)
+		val.Value("displayId").String().IsEqual(camp.DisplayID)
+		val.Value("name").String().IsEqual(camp.Name)
+		val.Value("description").String().IsEqual(camp.Description)
+		val.Value("isDraft").Boolean().IsEqual(camp.IsDraft)
+		val.Value("isPaymentOpen").Boolean().IsEqual(camp.IsPaymentOpen)
+		val.Value("isRegistrationOpen").Boolean().IsEqual(camp.IsRegistrationOpen)
+		val.Value("dateStart").String().IsEqual(camp.DateStart.Format(time.DateOnly))
+		val.Value("dateEnd").String().IsEqual(camp.DateEnd.Format(time.DateOnly))
+	})
+}
