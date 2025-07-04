86aaba55a0f0959359b0460edd5d51242007756b
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-07-05 02:41:45 +0900

    UpdateQuestionでOptionも更新できるように

--- repository/question.go
+++ repository/question.go
@@ -1,12 +1,16 @@
 //go:generate go tool mockgen -source=$GOFILE -destination=mock/$GOFILE -package=mock
 package repository
 
-import "github.com/traPtitech/rucQ/model"
+import (
+	"context"
+
+	"github.com/traPtitech/rucQ/model"
+)
 
 type QuestionRepository interface {
 	CreateQuestion(question *model.Question) error
 	GetQuestions() ([]model.Question, error)
 	GetQuestionByID(id uint) (*model.Question, error)
 	DeleteQuestionByID(id uint) error
-	UpdateQuestion(questionID uint, question *model.Question) error
+	UpdateQuestion(ctx context.Context, questionID uint, question model.Question) error
 }
