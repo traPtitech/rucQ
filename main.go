fe4aad3fbbc15136fd5a1177146aa583fa2655b2
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-08-06 23:44:32 +0900

    TRAQ_BOT_TOKENをTRAQ_BOT_ACCESS_TOKENに変更

--- main.go
+++ main.go
@@ -70,7 +70,7 @@ func main() {
 	repo := gormrepository.NewGormRepository(db)
 
 	traqBaseURL := os.Getenv("TRAQ_API_BASE_URL")
-	botAccessToken := os.Getenv("TRAQ_BOT_TOKEN")
+	botAccessToken := os.Getenv("TRAQ_BOT_ACCESS_TOKEN")
 	traqService := service.NewTraqService(traqBaseURL, botAccessToken)
 
 	api.RegisterHandlers(e, router.NewServer(repo, traqService, isDev))
