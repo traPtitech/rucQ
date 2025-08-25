d217e0739d499785cec1472ddd84965b11eed8b2
Takumi Akimoto <81888693+akimon658@users.noreply.github.com>
2025-08-25 13:24:22 +0900

    MessageRepositoryをGenerics APIでの実装に変更

--- repository/gormrepository/message.go
+++ repository/gormrepository/message.go
@@ -4,22 +4,26 @@ import (
 	"context"
 	"time"
 
+	"gorm.io/gorm"
+
 	"github.com/traPtitech/rucQ/model"
 )
 
 func (r *Repository) CreateMessage(ctx context.Context, message *model.Message) error {
-	return r.db.WithContext(ctx).Create(message).Error
+	return gorm.G[model.Message](r.db).Create(ctx, message)
 }
 
 func (r *Repository) GetReadyToSendMessages(ctx context.Context) ([]model.Message, error) {
-	var messages []model.Message
-	err := r.db.WithContext(ctx).
+	messages, err := gorm.G[model.Message](r.db).
 		Where("sent_at IS NULL").
 		Where("send_at <= ?", time.Now()).
-		Find(&messages).Error
+		Find(ctx)
+
 	return messages, err
 }
 
 func (r *Repository) UpdateMessage(ctx context.Context, message *model.Message) error {
-	return r.db.WithContext(ctx).Save(message).Error
+	_, err := gorm.G[*model.Message](r.db).Updates(ctx, message)
+
+	return err
 }
